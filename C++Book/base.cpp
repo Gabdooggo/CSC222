de9c040358fb6d6f2ca5a167f3c4bf314d28b791
Gabriel Callau Prellwitz <gabrielcallauprellwitz@Gabriels-MacBook-Air.local>
2026-09-03 09:49:25 -0400

    exercises

--- C++Book/base.cpp
+++ C++Book/base.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 //For 2. the value of 028 as a literal is invalid
 //For 5. The literal is invalid since an interger cannot contain a float it says.
+//For 11. The literal value is 16
+//For 13. The literal value is 0
+//For 15. The literal value is 35
+//For 16. The literal is invalid as there can't be a capital letter
 
 
 int main () {
-    int n = -2e-2;
+    int n = X10;
     cout << "Some bits represented in decimal: " << dec << n << '\n';
     cout << "Same bits represented in hexadecimal: " << hex << n << '\n';
     cout << "Same bits represented in octal: " << oct << n << '\n';
--- C++Book/scientific.cpp
+++ C++Book/scientific.cpp
@@ -6,16 +6,45 @@ using namespace std;
 //For 6. 4.2 is the correct float in C++
 //For 7. The utput is 7 6 6
 // For 8. The output is 13 -1
-
+//For 9. 2 is printed by the program
+//For 10. f is printed by the program
+//For 12. The value is 91211
+//For 14. bool is not a valid variable name because it is reserved for the data type
+//For 17. this_one_is is the only legal variable name in C++
+//For 18. The output is 31
+//For 19. E, F, and C are legal interger literal values in C++
+// For 20. The output is 20
+//For 21. The compilation fails as double == signs are needed.
 int main () {
-    float f = 4.0 / 8.0 + 8.0 / 16.0 + 8.0;
+   // float f;
+    //cin >> f;
+    //cout << scientific << "f";
+    //int n;
+    //cin >> n;
+    //int x = 3, y = x++, z = ++x;
+   // cin >> x;
+   // cout << z - x << y - x <<  endl;
+   //int x = 3, y = x++, z = ++x;
+    //cin >> x;
+   // cout << z - x << y - x <<  endl;
+   //int n = 2;
+   // cout << (n == "2") <<  endl;
+    //cout << n << hex << n + n << oct << n << endl;
      //int i = f; 
-     int i = 5, j = i++, k = ++i;
-     cin >> i;
-     cout << j + i << k - i << endl;
+     //int i = 5, j = i++, k = ++i;
+     //cin >> i;
+     //cout << j + i << k - i << endl;
      //int i = 5, j = ++i, k = i++;
      //cout << i << j << k << endl;
     //cout << f;
     //cout << scientific << f << "\n";
+    /*int i = 5, j, k;
+    if (i > 0) j = 2 + i * i;
+    if (i <= 0) j = 2 * i - 1;
+    if (j >= 0) k = j % i + 2;
+    if (j < 0) k = i % j + 2;
+    if (k < 0) k = k % i % j;
+    if (k >= 0) k = j % i % k;
+    cout << k << endl;*/
     return 0;
 }
